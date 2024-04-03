0098ada74590c8ffb56b1470a318696b3762d1e4
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-03 13:06:15 +0200

    Fix documentation typo.

--- R/mun_reg_saude.R
+++ R/mun_reg_saude.R
@@ -1,7 +1,7 @@
 #' Municipality and health region code table
 #'
 #' A dataset containing municipality and health regions codes.
-#'#'
+#'
 #' @format A tibble with 5,597 rows and 2 variables:
 #' \describe{
 #'   \item{code_muni}{municipality 6 digits code}
